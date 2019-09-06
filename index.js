61ba3b3bc597e4b336d23f0ecc235fd5662a6ba4
Anthony Rurup <tcrurup@gmail.com>
2019-09-06 23:32:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 
 
-newID = "10000000" + oldID
+newID = `10000000${oldID}`
 ageIsValid = Number.isInteger(currentAge)
